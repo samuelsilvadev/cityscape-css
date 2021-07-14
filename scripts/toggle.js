a872cb3b78d327e95e8795a6f2181f2b88a1c752
Samuel SIlva <samuelsilvawb@gmail.com>
2021-07-14 21:20:51 +0100

    feat: Build toggle

---
+++ scripts/toggle.js
@@ -0,0 +1,18 @@
+(() => {
+  const CLASSLIST = {
+    CHECKED: "checked"
+  };
+
+  const toggleLabel = document.querySelector('[data-script-id="toggle-label"]');
+  const toggleInput = document.querySelector('[data-script-id="toggle-input"]');
+
+  toggleInput.addEventListener("click", (event) => {
+    const isChecked = event.currentTarget.checked;
+
+    if (isChecked) {
+      toggleLabel.classList.add(CLASSLIST.CHECKED);
+    } else {
+      toggleLabel.classList.remove(CLASSLIST.CHECKED);
+    }
+  });
+})();
